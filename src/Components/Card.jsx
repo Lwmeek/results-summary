23e5b470ed9da6c1779a2832b5572ddf4d808c13
Timic Ivey <timicivey27@gmail.com>
2023-11-29 10:51:53 -0500

    Adding comments to code best practices

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -2,9 +2,9 @@ import * as React from "react";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 
-
 export default function BasicCard() {
 	return (
+		/* First Card with overall score */
 		<Card
 			sx={{
 				textAlign: "center",
--- src/Components/Card2.jsx
+++ src/Components/Card2.jsx
@@ -1,21 +1,12 @@
 import * as React from "react";
-import Box from "@mui/material/Box";
 import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Button from "@mui/material/Button";
 
-const bull = (
-	<Box
-		component="span"
-		sx={{ display: "inline-block", mx: "2px", transform: "scale(0.8)" }}
-	>
-		•
-	</Box>
-);
-
 export default function BasicCard2() {
 	return (
-		<Card sx={{ width: "100%", boxShadow:"none" }} className="card2">
+		/* Second Card with score of each section */
+		<Card sx={{ width: "100%", boxShadow: "none" }} className="card2">
 			<CardContent sx={{ width: "85%", margin: " 0 auto" }}>
 				<h3>Summary</h3>
 				<div className="reaction score-card">
@@ -47,7 +38,7 @@ export default function BasicCard2() {
 					<p className="score">72 / 100</p>
 				</div>
 			</CardContent>
-
+			{/* Button */}
 			<Button
 				variant="contained"
 				sx={{
